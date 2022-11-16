7b58990befaa76200481bb37356c263dcd3bc799
eugeneburdukov <eugenebkv@yahoo.com.ua>
2022-11-16 17:42:18 +0200

    added failed test

--- src/test/java/LempopoTest.java
+++ src/test/java/LempopoTest.java
@@ -3,8 +3,12 @@ import org.testng.annotations.Test;
 
 public class LempopoTest {
 
-    @Test(priority = 1, description = "Lempopo testing")
+    @Test(priority = 1, description = "Limpopo testing 1")
     public void doConcat(){
         Assert.assertEquals("review","review");
+    }
+    @Test(priority = 2, description = "Limpopo testing 2")
+    public void doReview(){
+        Assert.assertEquals("epitome","epiphany");
     }
 }
